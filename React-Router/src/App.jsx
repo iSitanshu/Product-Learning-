82ab21e5887487a86b45863b5b5659bbbcbc70a7
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-05 01:32:02 +0530

    Structure

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import React from "react"
+import Home from "./components/Home/Home"
+import Layout from "./components/Layout"
+import Header from "./components/Header/Header"
+
+export default function App() {
+  return (
+    <>
+    </>
+  )
+}
