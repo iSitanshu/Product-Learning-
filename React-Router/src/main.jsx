672e9fffe15c71d2c0972d806a9f8e33e6c696bc
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-05 05:42:27 +0530

    main

--- React-Router/src/main.jsx
+++ React-Router/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode>mm ckvndk nvl kvnlkbln
     <App />
   </React.StrictMode>,
 )
